4914c0dadeb6ada638b7a46b0ac2923bf9d3f101
phukhieo <akopadung>
2020-04-03 13:39:52 +0700

    Updates

---
+++ precache-manifest.3121794d6ce58ba42d3e3694d3de0275.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d62c3614e9ad72b4779cd8bb0f0b78",
+    "url": "/test-classroom/index.html"
+  },
+  {
+    "revision": "ec989bd78f20a4354bc3",
+    "url": "/test-classroom/static/css/main.563b42bf.chunk.css"
+  },
+  {
+    "revision": "84f2ad47fcf33f9e67fb",
+    "url": "/test-classroom/static/js/2.d8bf0f94.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test-classroom/static/js/2.d8bf0f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec989bd78f20a4354bc3",
+    "url": "/test-classroom/static/js/main.9a9cee4c.chunk.js"
+  },
+  {
+    "revision": "6dd6050786c2a44cd5c6",
+    "url": "/test-classroom/static/js/runtime-main.21ab2fd6.js"
+  },
+  {
+    "revision": "8eaa53ab12737df476ec37173e40ac6e",
+    "url": "/test-classroom/static/media/about.8eaa53ab.jpg"
+  },
+  {
+    "revision": "fb95d7130f66e3792136167b39aed951",
+    "url": "/test-classroom/static/media/img_bg_slide0.fb95d713.jpg"
+  },
+  {
+    "revision": "2e01b436a72d0a351efcaf820b98cfa6",
+    "url": "/test-classroom/static/media/img_bg_slide_1.2e01b436.JPG"
+  },
+  {
+    "revision": "5bbce33e2725f3d5888fc2ac52ee54f1",
+    "url": "/test-classroom/static/media/img_bg_slide_2.5bbce33e.jpg"
+  }
+]);
